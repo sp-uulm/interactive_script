6d49ef169b48df7ce0bdd3195737b150127c9d6b
Thomas Witte <thomas.witte@uni-ulm.de>
2019-12-12 16:01:06 +0100

    added workspace blockly repository

--- interactive_script/include/interactive_script/interactive_script_plugin.h
+++ interactive_script/include/interactive_script/interactive_script_plugin.h
@@ -24,12 +24,12 @@ class InteractiveScriptGui
 
 public:
     InteractiveScriptGui();
-    virtual void initPlugin(qt_gui_cpp::PluginContext& context);
-    virtual void shutdownPlugin();
+    virtual void initPlugin(qt_gui_cpp::PluginContext& context) override;
+    virtual void shutdownPlugin() override;
     virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings,
-      qt_gui_cpp::Settings& instance_settings) const;
+      qt_gui_cpp::Settings& instance_settings) const override;
     virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings,
-      const qt_gui_cpp::Settings& instance_settings);
+      const qt_gui_cpp::Settings& instance_settings) override;
 
     void execute_vis();
     void removeFormatting();
--- interactive_script/src/interactive_script_plugin/blocklywidget.cpp
+++ interactive_script/src/interactive_script_plugin/blocklywidget.cpp
@@ -1,8 +1,15 @@
 #include "interactive_script/blocklywidget.h"
+#include <ament_index_cpp/get_package_share_directory.hpp>
 
 BlocklyWidget::BlocklyWidget(QWidget *parent) : QWebEngineView(parent)
 {
-    load(QUrl("file:///home/thomas/catkin_ws/src/blockly/index.html"));
+    QString path = "file://"
+         + QString::fromStdString(ament_index_cpp::get_package_share_directory("interactive_script_blockly"))
+         + "/../quadcopter/index.html";
+         
+    std::cout << "Loading blockly URL: " << path.toStdString() << std::endl;
+         
+    load(QUrl(path));
 
     bridge = new BlocklyBridge();
     channel = new QWebChannel(this);
